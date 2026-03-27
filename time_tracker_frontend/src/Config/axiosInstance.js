7f35cb0b857fdc9096ef3c998195826e24472218
Ana-Maria Corcodel <anamaria.corcodel272004@yahoo.com>
2026-03-27 10:49:09 +0200

    am reparat axios pentru a merge adaugarea angajatilor din frontend

--- time_tracker_frontend/src/Config/axiosInstance.js
+++ time_tracker_frontend/src/Config/axiosInstance.js
@@ -1,11 +1,30 @@
 import axios from 'axios';
 
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) {
+    return parts.pop().split(';').shift();
+  }
+  return null;
+}
+
 const axiosInstance = axios.create({
   baseURL: 'http://localhost:8000',
+  withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
   },
-  withCredentials: true,
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  const csrfToken = getCookie('csrftoken');
+
+  if (csrfToken) {
+    config.headers['X-CSRFToken'] = csrfToken;
+  }
+
+  return config;
 });
 
 export default axiosInstance;
